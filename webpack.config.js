12478ac8085812c8d527b6399fe10150e33919d2
soszaboss <moustoifa10@outlook.fr>
2024-03-16 13:14:48 +0000

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+    entry: './assets/scripts/index.js',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'static', 'js')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            }
+        ]
+    }
+}
